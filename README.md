31cc93b5be0bd2ff022f9545b8566187f7ba2449
Luka Rantalainen <130208104+lukarantalainen@users.noreply.github.com>
2025-04-14 08:56:44 +0000

    HEIIIIIIIKIIJIIIIOII

--- README.md
+++ README.md
@@ -36,3 +36,11 @@ npm run build
 You can preview the production build with `npm run preview`.
 
 > To deploy your app, you may need to install an [adapter](https://svelte.dev/docs/kit/adapters) for your target environment.
+
+
+git add .
+git commit -m "viesti"
+git push origin master
+
+npm run dev
+
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,26 @@
+<div id="header">
+    <h1>frii.bet</h1>
+    <a>peli1</a>
+    <a>peli2</a>
+  </div>
+  
+  
+  <style>
+    #header {
+      display: flex;
+      background-color: red;
+      align-items: center;
+      align-content: center;
+      justify-content: left
+    }
+  
+  
+    #header * {
+      margin: 0px;
+      padding: 0px;
+    }
+  
+    #header a {
+    margin-left: 2rem;
+    }
+  </style>
