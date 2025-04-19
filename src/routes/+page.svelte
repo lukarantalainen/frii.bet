fccb6aad7fc50556ac93977c57c4fcddfde03f76
Luka Rantalainen <130208104+lukarantalainen@users.noreply.github.com>
2025-04-19 18:00:15 +0000

    added navigation buttons

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,9 +5,19 @@
 </div>
 
 <div id=games>
+    <div class=game1> 
+        <a href="/p2">Crash</a>
+    </div>
 
+    
+    <div class=game2>
+        <a href="/p3">Dice</a>
+    </div>
+    
 
 </div>
+
+
 
 <style>
 
@@ -21,6 +31,7 @@
         padding-left: 8px;
     }
 
+
     h1 {
     color: #FFFFFF;
     text-decoration: none;
@@ -32,6 +43,21 @@
         height: 100vh;
         border-radius: 10px;
         margin-left: 10px;
+    }
+
+    #games div {
+        background-color: #212738;
+        width: 200px;
+        height: 150px;
+        border-radius: 10px;
+        display: grid;
+    }
+
+    #games a {
+        color: blue;
+        text-align: center;
+        text-decoration: none;
+        font-size: 30px;
     }
 
 </style>
